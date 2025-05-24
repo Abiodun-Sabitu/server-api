4c56b3f59f8dd20bdb49e56345b02d943cd8ae9a
Sabmanny <95466606+Abiodun-Sabitu@users.noreply.github.com>
2025-05-24 14:28:49 +0100

    Add nodemon Add index.html page Add 404 Pag Create Get all items utils Create generate ID utils

--- catalogue/items.js
+++ catalogue/items.js
@@ -1,3 +1,12 @@
 [
-
+  { "id": 1, "name": "Banana", "price": 150, "size": "s" },
+  { "id": 2, "name": "Avocado", "price": 300, "size": "m" },
+  { "id": 3, "name": "Yam", "price": 1200, "size": "l" },
+  { "id": 4, "name": "Tomato Basket", "price": 700, "size": "m" },
+  { "id": 5, "name": "Onion Sack", "price": 1800, "size": "l" },
+  { "id": 6, "name": "Carrot Pack", "price": 250, "size": "s" },
+  { "id": 7, "name": "Cabbage Head", "price": 500, "size": "m" },
+  { "id": 8, "name": "Eggs (Crate)", "price": 2000, "size": "l" },
+  { "id": 9, "name": "Pepper Mix", "price": 350, "size": "s" },
+  { "id": 10, "name": "Sweet Potato", "price": 900, "size": "m" }
 ]
